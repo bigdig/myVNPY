06bdaf836403e61bf72cc4289620d4b44f7a6328
yesheng <yesheng1984@139.com>
2017-04-14 12:15:11 +0800

    增加plot功能

--- arbitrage.py
+++ arbitrage.py
@@ -1,9 +1,12 @@
-# encoding: UTF - 8
+# encoding: UTF-8
+# auther: Y.Raul
+
 # 系统包导入
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from statsmodels.tsa.stattools import adfuller
+from datetime import datetime
 
 # 绘图包导入
 import  matplotlib.pyplot as plt
@@ -12,7 +15,7 @@ import seaborn as sns
 #分析包导入
 import talib as ta
 
-class Arbitrage(object):
+class FindPairs(object):
     """
     套利分析类，完成以下功能：
     1、检验协整性
@@ -31,13 +34,14 @@ class Arbitrage(object):
         self.pvalue_matrix = np.ones((self.n, self.n))
         self.stationarityRes = pd.DataFrame()
         self.keys = data.keys()
-        self.test_stationarity()
+        self.find_stationarity()
         self.find_cointegration()
+        self.find_coef()
 
-    def test_stationarity(self):
+    def find_stationarity(self):
         """
-        
-        :return: 
+        平稳性检验，p-value小于0.05则认为平稳
+        :return: stationarityRes中保留检验结果
         """
         for sub in self.data.columns:
             # print sub
@@ -51,8 +55,8 @@ class Arbitrage(object):
 
     def find_cointegration(self):
         """
-        
-        :return: 
+        协整性检验，p-value小于0.05则认为平稳。
+        :return: pairs中保存配对结果
         """
         pairs = []
         for i in range(self.n):
@@ -64,17 +68,19 @@ class Arbitrage(object):
                 self.pvalue_matrix[i, j] = pvalue
                 if pvalue < 0.05:
                     pairs.append((self.keys[i], self.keys[j], pvalue))
+
+        # pair配对结果
         self.pairs = pd.DataFrame(pairs, columns=['leg1','leg2','pvalue'])
-        self.pairs.sort_index(by = 'pvalue', inplace=True)
+        self.pairs.sort_values(by = 'pvalue', inplace=True)
         index = np.arange(len(self.pairs))
 
         self.pairs['index'] = index
         self.pairs.set_index('index', inplace= True)
 
-    def find_ols(self):
+    def find_coef(self):
         """
-        
-        :return: 
+        计算配对系数coef
+        :return: pairs中增加配对系数coef列，对应pair的spread为 (coef*leg1 - leg2)
         """
         coefList = []
         for line in range(len(self.pairs)):
@@ -93,9 +99,9 @@ class Arbitrage(object):
 
         self.pairs['coef'] = coefList
 
-    def plotHM(self):
+    def plotHeatmap(self):
         """
-        
+        根据协整性检验的结果绘制热力图
         :return: 
         """
         # print type(self.data.columns.values)
@@ -105,12 +111,70 @@ class Arbitrage(object):
         # sns.heatmap(1 - self.pvalue_matrix)
         plt.show()
 
-    def plotPair(self, leg1, leg2):
+    def plotPair(self, leg1, leg2,mul = 2 ):
         """
-        
+        绘制leg1,leg2走势图，spread价差回归图，spread价差分布图
+        :param leg1: 
+        :param leg2: 
+        :param mul: spread的上下限标准差倍数，默认为2
         :return: 
         """
-        pass
+        tmp = self.pairs[self.pairs.leg1 == leg1]
+        coef =  (tmp[tmp.leg2 == leg2].coef).values
+        # print coef
+
+        legs = pd.DataFrame()
+        legs[leg1] = self.data[leg1]
+        legs[leg2] = self.data[leg2]
+        legs['spread'] = (coef * legs[leg1] - legs[leg2])
+        spreadMean = legs['spread'].mean()
+        spreadStd = legs['spread'].std()
+
+        # spreadZ为价差的标准化Z分数
+        legs['spreadZ'] = (legs['spread'] - spreadMean)/spreadStd
+        idmax = legs['spread'].idxmax()
+        idmin = legs['spread'].idxmin()
+
+
+
+        print  "value of max: ", legs['spread'].ix[idmax]
+        print  "value of min: ", legs['spread'].ix[idmin]
+
+        # print type(idmax)
+        # 绘图初始设置
+        fig, axes = plt.subplots(3, 1, figsize=(20, 12))
+
+        # 绘制leg1,leg2走势图
+        legs[[leg1,leg2]].plot(ax = axes[0])
+        axes[0].grid(True)
+        axes[0].set_ylabel('Settle Price')
+        axes[0].set_title(leg1 + u' and ' + leg2)
+
+        # 绘制价差spread走势图
+        # spreadMid = spreadMean
+        spreadUp = spreadMean + mul* spreadStd
+        spreadDown = spreadMean - mul* spreadStd
+
+        legs['spread'].plot(ax = axes[1], color = 'k')
+        axes[1].axhline(spreadMean, color="red", linestyle="--")
+        axes[1].axhline(spreadUp, color="b", linestyle="--")
+        axes[1].axhline(spreadDown, color="b", linestyle="--")
+
+        axes[1].legend(["Spread", "Mean"])
+        axes[1].set_ylabel('Spread Price')
+        outlier = [(idmax, round(legs['spread'].ix[idmax],2)), (idmin, round(legs['spread'].ix[idmin],2))]
+
+        spread = legs['spread']
+        for date,label in outlier:
+            axes[1].annotate(label, xy=(date, spread.asof(date)), xytext = (date, spread.asof(date) + 200),
+                              arrowprops=dict(facecolor='R'),
+                              horizontalalignment='left', verticalalignment='top')
+        # 绘制spread的分布图
+        sns.distplot(legs['spreadZ'], bins=50, ax = axes[2])
+        axes[2].set_ylabel('Hist')
+        # legs['spread'].hist(bins = 50, ax =axes[2])
+        plt.subplots_adjust(hspace=0.3)
+        plt.show()
 
 if __name__=='__main__':
 
@@ -135,32 +199,17 @@ if __name__=='__main__':
     badFluidity = ['WH','RI','LR','JR','FB','BB','PB','SF','SM','SN','BU','WR']
     for i in badFluidity:
         del df[i]
+
     # print df.info()
+    # 将index更换为DatetimeIndex
+    df.set_index(pd.to_datetime(df.index.values), inplace=True)
+    # print df.info()
+
+    # df.dropna(inplace=True)
     # print df['JD']
-    test = Arbitrage(df.dropna())
-    # test.plotHM()
-    # res1 = test.stationarityRes.T
-    # print res1.ix['PM']
-    # print res1[res1['p-value'] < 0.05]
-    # print test.stationarityRes['PM']
-    # print test.pvalue_matrix
-    # print test.pairs.iloc[0]['leg1']
-    test.find_ols()
-    # print test.pairs
 
+    test = FindPairs(df.dropna())
+    print test.pairs
     leg1 = 'PM'
-    leg2 = 'IF'
-    coef = test.pairs[test.pairs.leg1 == 'PM'][test.pairs.leg2 == 'IF'].coef
-    # print coef
-    spread = (coef.values*df[leg1] - df[leg2])
-    spreadDF = pd.DataFrame(spread).dropna()
-
-    # print type(spread)
-    spreadDF.plot()
-
-    plt.axhline(spread.mean(), color="red", linestyle="--")
-    plt.xlabel("Time");
-    plt.ylabel("Stationary Series")
-    plt.legend(["Stationary Series", "Mean"])
-    plt.title(leg1 +'---' + leg2)
-    plt.show()
+    leg2 = 'M'
+    test.plotPair(leg1,leg2)
